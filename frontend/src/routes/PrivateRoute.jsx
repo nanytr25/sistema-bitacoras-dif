6c806e4e13aeb6adf9a60acd81a7974f91989557
Nancy Guadalupe Robles Trejo <nanytr259@gmail.com>
2026-08-27 02:01:25 -0600

    Configurar API_URL con variable de entorno para producción3

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,8 +1,16 @@
 import { Navigate } from "react-router-dom";
-
+import { useEffect} from "react";
 
 function PrivateRoute({ children }) {
-
+  useEffect(() => {
+    const manejarPageShow = (event) => {
+      if (event.persisted) {
+        window.location.reload();
+      }
+    };
+    window.addEventListener("pageshow", manejarPageShow);
+    return () => window.removeEventListener("pageshow", manejarPageShow);
+  }, []);
   const token = localStorage.getItem("token");
 
 
